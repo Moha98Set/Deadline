b34b637751882216d4658490dd95bb20156cd8b1
Moha98Set <153164170+Moha98Set@users.noreply.github.com>
2025-03-15 21:03:10 +0330

    ready 2

---
+++ app/viewMap/[id]/page.jsx
@@ -0,0 +1,28 @@
+import ViewMap from "../page"
+
+const getDataById = async (id) =>{
+    try {
+        const res = await fetch(`http://localhost:3000/api/data/${id}`, {cache: 'no-store'})
+
+        if(!res.ok){
+            throw new Error('Failed to fetch Data.')
+        }
+        console.log(res)
+        return res.json()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function ShowMap({ params }){
+    const { id } = params
+    const { data } = await getDataById(id)    
+    return(
+        <>            
+            <ViewMap data={data.coordinates[0]} />            
+        </>
+    )
+
+}
+
+export default ShowMap
